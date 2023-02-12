98c4b6c3a01cb2f2882ea8788a22de1784c6e594
Radouane Iben-hamou <redouanebh4@gmail.com>
2023-02-12 22:01:51 +0100

    pipex

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: reben-ha <reben-ha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 23:26:08 by reben-ha          #+#    #+#             */
-/*   Updated: 2023/02/12 20:26:44 by reben-ha         ###   ########.fr       */
+/*   Updated: 2023/02/12 22:00:56 by reben-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,23 +30,24 @@ void    ft_error(int status, int code)
 
 int	main(int argc, char **argv)
 {
-	void	*mlx;
-	void	*mlx_win;
-	void	*img;
+	t_image	img;
 	char	**map;
+	int		len;
 
-	// if (argc <= 1)
-	// 	return (ft_printf(2, "Error : Invalid argument\n"), 1);
-	// map = ft_input_manage(argv[1]);
-
-	mlx = mlx_init();
-
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "So_long REDMEGA-Edition");
-
+	if (argc <= 1)
+		return (ft_printf(2, "Error : Invalid argument\n"), 1);
+	map = ft_input_manage(argv[1]);
+	img.x = ft_strlen(map[0]);
+	img.y = 0;
+	while (map[img.y])
+		img.y++;
+	img.mlx = mlx_init();
+	img.mlx_win = mlx_new_window(img.mlx, img.x * 50, img.y * 50, "So_long REDMEGA-Edition");
+	
 	// img = mlx_xpm_to_image(mlx, ); //
 	
 	// mlx_put_image_to_window(mlx, mlx_win, img, );
 
-	mlx_loop(mlx);
+	mlx_loop(img.mlx);
 	// system("leaks so_long");
 }
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: reben-ha <reben-ha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 23:26:10 by reben-ha          #+#    #+#             */
-/*   Updated: 2023/02/12 20:25:37 by reben-ha         ###   ########.fr       */
+/*   Updated: 2023/02/12 20:40:06 by reben-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,16 @@ typedef struct s_struct
 	int		i;
 	int		j;
 }			t_loc;
+
+typedef struct s_image
+{
+	int		x;
+	int		y;
+	void	*mlx;
+	void	*mlx_win;
+	void	*img;
+}			t_image;
+
 
 void    ft_error(int status, int code);
 void	ft_free(char **str);
