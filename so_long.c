905643c9c0065fe188069a9c92c178976f82af70
Radouane Iben-hamou <redouanebh4@gmail.com>
2023-02-16 23:33:21 +0100

    so_long

--- so_long.c
+++ so_long.c
@@ -24,23 +24,23 @@ void	ft_error_str(void *status, int code)
 
 void	ft_open_image(t_mlx_win *mlx_x, int *width, int *height)
 {
-	mlx_x->img.player.front = mlx_xpm_file_to_image(mlx_x->mlx, "texture/sprites/player/front.xpm", width, height);
+	mlx_x->img.player.front = mlx_xpm_file_to_image(mlx_x->mlx, "texture/player/3-front.xpm", width, height);
 	ft_error_str(mlx_x->img.player.front, 1);
-	mlx_x->img.player.back = mlx_xpm_file_to_image(mlx_x->mlx, "texture/sprites/player/back.xpm", width, height);
+	mlx_x->img.player.back = mlx_xpm_file_to_image(mlx_x->mlx, "texture/player/3-back.xpm", width, height);
 	ft_error_str(mlx_x->img.player.back, 1);
-	mlx_x->img.player.right = mlx_xpm_file_to_image(mlx_x->mlx, "texture/sprites/player/right.xpm", width, height);
+	mlx_x->img.player.right = mlx_xpm_file_to_image(mlx_x->mlx, "texture/player/3-right.xpm", width, height);
 	ft_error_str(mlx_x->img.player.right, 1);
-	mlx_x->img.player.left = mlx_xpm_file_to_image(mlx_x->mlx, "texture/sprites/player/left.xpm", width, height);
+	mlx_x->img.player.left = mlx_xpm_file_to_image(mlx_x->mlx, "texture/player/3-left.xpm", width, height);
 	ft_error_str(mlx_x->img.player.left, 1);
-	mlx_x->img.floor = mlx_xpm_file_to_image(mlx_x->mlx, "texture/sprites/floor.xpm", width, height);
+	mlx_x->img.floor = mlx_xpm_file_to_image(mlx_x->mlx, "texture/floor.xpm", width, height);
 	ft_error_str(mlx_x->img.floor, 1);
-	mlx_x->img.wall = mlx_xpm_file_to_image(mlx_x->mlx, "texture/sprites/wall.xpm", width, height);
+	mlx_x->img.wall = mlx_xpm_file_to_image(mlx_x->mlx, "texture/wall.xpm", width, height);
 	ft_error_str(mlx_x->img.wall, 1);
-	mlx_x->img.coin = mlx_xpm_file_to_image(mlx_x->mlx, "texture/sprites/coin.xpm", width, height);
+	mlx_x->img.coin = mlx_xpm_file_to_image(mlx_x->mlx, "texture/coin.xpm", width, height);
 	ft_error_str(mlx_x->img.coin, 1);
-	mlx_x->img.exit = mlx_xpm_file_to_image(mlx_x->mlx, "texture/sprites/exit.xpm", width, height);
+	mlx_x->img.exit = mlx_xpm_file_to_image(mlx_x->mlx, "texture/door/exit.xpm", width, height);
 	ft_error_str(mlx_x->img.exit, 1);
-	mlx_x->img.enemy = mlx_xpm_file_to_image(mlx_x->mlx, "texture/sprites/enemy.xpm", width, height);
+	mlx_x->img.enemy = mlx_xpm_file_to_image(mlx_x->mlx, "texture/enemy_normal.xpm", width, height);
 	ft_error_str(mlx_x->img.enemy, 1);
 }
 
