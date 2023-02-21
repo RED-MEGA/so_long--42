c16896181b009a5d63369d44723b5f9c594882cc
Radouane Iben-hamou <redouanebh4@gmail.com>
2023-02-21 18:52:20 +0100

    so_long

--- so_long.c
+++ so_long.c
@@ -36,6 +36,31 @@ void	ft_select_img(t_mlx_win *mlx_x, char *option)
 		return ;
 }
 
+void	ft_destroy(t_mlx_win *mlx_x)
+{
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.coin);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.enemy);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.enemy_angry);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.exit);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.exit_2);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.floor);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.wall);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.player.back);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.player.front);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.player.left);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.player.right);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.exit_hole.frame_1);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.exit_hole.frame_2);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.exit_hole.frame_3);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.exit_hole.frame_4);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.exit_hole.frame_5);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.exit_hole.frame_6);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.exit_hole.frame_7);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.exit_hole.frame_8);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.exit_hole.frame_9);
+	mlx_destroy_image(mlx_x->mlx, mlx_x->img.exit_hole.frame_10);
+}
+
 int	main(int argc, char **argv)
 {
 	t_mlx_win	mlx_x;
