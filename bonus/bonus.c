56107ad7338e1641125c2393642234504925cbac
Radouane Iben-hamou <redouanebh4@gmail.com>
2023-02-24 19:43:01 +0100

    so_long

--- bonus/bonus.c
+++ bonus/bonus.c
@@ -6,11 +6,18 @@
 /*   By: reben-ha <reben-ha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 05:32:04 by reben-ha          #+#    #+#             */
-/*   Updated: 2023/02/22 16:19:42 by reben-ha         ###   ########.fr       */
+/*   Updated: 2023/02/24 19:42:17 by reben-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
+
+void	select_img_hole(t_mlx_win *mlx_x, char *img_ch, t_loc loc)
+{
+	mlx_do_sync(mlx_x->mlx);
+	mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, img_ch,
+		loc.j * 100, loc.i * 100);
+}
 
 void	midgard_hole(t_mlx_win *mlx_x)
 {
@@ -18,61 +25,30 @@ void	midgard_hole(t_mlx_win *mlx_x)
 
 	loc = fp(mlx_x->map, 'E', 'l');
 	if (mlx_x->time_midgard <= 14 && fp(mlx_x->map, 'C', 'n').i == 1)
-	{
-		mlx_do_sync(mlx_x->mlx);
-		mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, mlx_x->img.exit_2, loc.j * 100, loc.i * 100);
-	}
+		select_img_hole(mlx_x, mlx_x->img.exit_2, loc);
 	else if (mlx_x->time_midgard <= 2)
-	{
-		mlx_do_sync(mlx_x->mlx);
-		mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, mlx_x->img.exit_hole.frame_1, loc.j * 100, loc.i * 100);
-	}
+		select_img_hole(mlx_x, mlx_x->img.exit_hole.frame_1, loc);
 	else if (mlx_x->time_midgard <= (2 * 2))
-	{
-		mlx_do_sync(mlx_x->mlx);
-		mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, mlx_x->img.exit_hole.frame_2, loc.j * 100, loc.i * 100);
-	}
+		select_img_hole(mlx_x, mlx_x->img.exit_hole.frame_2, loc);
 	else if (mlx_x->time_midgard <= (2 * 3))
-	{
-		mlx_do_sync(mlx_x->mlx);
-		mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, mlx_x->img.exit_hole.frame_3, loc.j * 100, loc.i * 100);
-	}
+		select_img_hole(mlx_x, mlx_x->img.exit_hole.frame_3, loc);
 	else if (mlx_x->time_midgard <= (2 * 4))
-	{
-		mlx_do_sync(mlx_x->mlx);
-		mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, mlx_x->img.exit_hole.frame_4, loc.j * 100, loc.i * 100);
-	}
+		select_img_hole(mlx_x, mlx_x->img.exit_hole.frame_4, loc);
 	else if (mlx_x->time_midgard <= (2 * 5))
-	{
-		mlx_do_sync(mlx_x->mlx);
-		mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, mlx_x->img.exit_hole.frame_5, loc.j * 100, loc.i * 100);
-	}
+		select_img_hole(mlx_x, mlx_x->img.exit_hole.frame_5, loc);
 	else if (mlx_x->time_midgard <= (2 * 6))
-	{
-		mlx_do_sync(mlx_x->mlx);
-		mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, mlx_x->img.exit_hole.frame_6, loc.j * 100, loc.i * 100);
-	}
+		select_img_hole(mlx_x, mlx_x->img.exit_hole.frame_6, loc);
 	else if (mlx_x->time_midgard <= (2 * 7))
-	{
-		mlx_do_sync(mlx_x->mlx);
-		mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, mlx_x->img.exit_hole.frame_7, loc.j * 100, loc.i * 100);
-	}
+		select_img_hole(mlx_x, mlx_x->img.exit_hole.frame_7, loc);
 	else if (mlx_x->time_midgard <= (2 * 8))
-	{
-		mlx_do_sync(mlx_x->mlx);
-		mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, mlx_x->img.exit_hole.frame_8, loc.j * 100, loc.i * 100);
-	}
+		select_img_hole(mlx_x, mlx_x->img.exit_hole.frame_8, loc);
 	else if (mlx_x->time_midgard <= (2 * 9))
-	{
-		mlx_do_sync(mlx_x->mlx);
-		mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, mlx_x->img.exit_hole.frame_9, loc.j * 100, loc.i * 100);
-	}
+		select_img_hole(mlx_x, mlx_x->img.exit_hole.frame_9, loc);
 	else if (mlx_x->time_midgard <= (2 * 10))
 	{
-		mlx_do_sync(mlx_x->mlx);
-		mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, mlx_x->img.exit_hole.frame_10, loc.j * 100, loc.i * 100);
 		if (mlx_x->time_midgard == (2 * 10))
 			mlx_x->time_midgard = 0;
+		select_img_hole(mlx_x, mlx_x->img.exit_hole.frame_10, loc);
 	}
 	mlx_x->time_midgard++;
 }
@@ -82,7 +58,7 @@ void	enemy_animated(t_mlx_win *mlx_x)
 	t_loc		loc;
 	void		*img_enm;
 	void		*img_flo;
-	static		int i;
+	static int	i;
 
 	loc = fp(mlx_x->map, 'T', 'l');
 	if (loc.i == -1)
@@ -98,24 +74,30 @@ void	enemy_animated(t_mlx_win *mlx_x)
 	if (mlx_x->map[loc.i][loc.j + 1] != '1' && i == 0)
 	{
 		if (mlx_x->map[loc.i][loc.j + 1] == 'P')
-			return (ft_printf(1, "\033[1;31mYOU ARE DEAD\n"), ft_destroy(mlx_x), exit(0));
+			return (ft_printf(1, "\033[1;31mYOU ARE DEAD\n"),
+				ft_destroy(mlx_x), exit(0));
 		mlx_do_sync(mlx_x->mlx);
 		mlx_x->map[loc.i][loc.j] = '0';
-		mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, img_flo, loc.j * 100, loc.i * 100);
+		mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, img_flo,
+			loc.j * 100, loc.i * 100);
 		mlx_x->map[loc.i][loc.j + 1] = 'T';
-		mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, img_enm, (loc.j + 1) * 100, loc.i * 100);
+		mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, img_enm,
+			(loc.j + 1) * 100, loc.i * 100);
 		if (mlx_x->map[loc.i][loc.j + 2] == '1')
 			i = 1;
 	}
 	else if (mlx_x->map[loc.i][loc.j - 1] != '1' && i == 1)
 	{
 		if (mlx_x->map[loc.i][loc.j - 1] == 'P')
-			return (ft_printf(1, "\033[1;31mYOU ARE DEAD\n"), ft_destroy(mlx_x), exit(0));
+			return (ft_printf(1, "\033[1;31mYOU ARE DEAD\n"),
+				ft_destroy(mlx_x), exit(0));
 		mlx_do_sync(mlx_x->mlx);
 		mlx_x->map[loc.i][loc.j] = '0';
-		mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, img_flo, loc.j * 100, loc.i * 100);
+		mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, img_flo,
+			loc.j * 100, loc.i * 100);
 		mlx_x->map[loc.i][loc.j - 1] = 'T';
-		mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, img_enm, (loc.j - 1) * 100, loc.i * 100);
+		mlx_put_image_to_window(mlx_x->mlx, mlx_x->mlx_win, img_enm,
+			(loc.j - 1) * 100, loc.i * 100);
 		if (mlx_x->map[loc.i][loc.j - 2] == '1')
 			i = 0;
 	}
